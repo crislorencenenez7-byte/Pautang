e8e2cd634a0f93d001633cb2bbd6781f1b076007
crislorencenenez7-byte <crislorencenenez7@gmail.com>
2026-09-25 20:48:59 +0800

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // Fill this with your Firebase Web App configuration.
 // Do not put service-account credentials here.
 window.PAUTANG_FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB46VTbz_5qLFh6QEoE2Y3XMMiMRClnIGc",
+  authDomain: "pautangmo-fb339.firebaseapp.com",
+  projectId: "pautangmo-fb339",
+  storageBucket: "pautangmo-fb339.firebasestorage.app",
+  messagingSenderId: "42005556721",
+  appId: "1:42005556721:web:937c75db0bdea18393a850",
+  measurementId: "G-RS6Q8KVZS1"
 };
