86ade6536a54059bab62add8630000ef75a5d640
crislorencenenez7-byte <crislorencenenez7@gmail.com>
2026-09-26 18:35:32 +0800

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification, updateProfile, signOut } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const config = window.PAUTANG_FIREBASE_CONFIG;
+if (!config || !config.apiKey) throw new Error("Firebase configuration is missing.");
+const app = initializeApp(config);
+export const auth = getAuth(app);
+export { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification, updateProfile, signOut };
