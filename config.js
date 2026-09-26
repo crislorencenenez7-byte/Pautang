b7f1af853b88ee7192f1513d922e800223a94e80
crislorencenenez7-byte <crislorencenenez7@gmail.com>
2026-09-26 17:12:36 +0800

    Update GOOGLE_SHEET_URL in config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
   Do NOT put Firebase service-account keys, admin passwords, or private keys here.
 */
 window.PAUTANG_CONFIG = {
-  GOOGLE_SHEET_URL: "https://script.google.com/macros/s/AKfycbwHs0s9C-vgLEskjs1JPIuSE8nKsYdI7nc98jdzSO4m0LE6qlXwTjsU4OIZkB-wqsPo/exec",
+  GOOGLE_SHEET_URL: "https://script.google.com/macros/s/AKfycbzbytHL3BGV02ca4nJczLwOs2rA9ur1Ny4z47V_iEOfcl2incLNVMsO3yC2ZL4rEwzi/exec",
 
   // Replace these placeholders with the GCash account that should receive payments.
   // These values are public on the payment page.
